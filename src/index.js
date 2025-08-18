815357004128af14d95dc62b8d6150940605ed75
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-08-18 14:45:11 +0700

    Webpack running successfully

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import { greeting } from "./greeting.js";
 import "./style.css";
-import profileImage from "./profile.jpg";
 
-const image = document.createElement("img");
-image.src = profileImage;
 
-document.body.appendChild(image);
-
-console.log(greeting);
+console.log("Hello World");
